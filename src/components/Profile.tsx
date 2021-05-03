f8c8e22e6d4c51f788906d2480465ebc0e472666
atalo pereira <atalo@geowellex.com>
2021-05-03 18:48:34 -0300

    Created context for countdown

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/atalopereira.png" alt="image profile" />
@@ -8,7 +11,7 @@ export function Profile() {
         <strong>Átalo Pereira</strong>
         <p>
           <img src="icons/level.svg" alt="level"/>
-          level 1
+          level {level}
         </p>
       </div>
     </div>
