7ed1fda0dd1d87e1246c149df26984891f7498e7
atalo pereira <atalo@geowellex.com>
2021-05-25 20:36:42 -0300

    Styled the classification user page

--- src/pages/classificationUsers.tsx
+++ src/pages/classificationUsers.tsx
@@ -1,8 +1,38 @@
+import styles from  '../styles/pages/ClassificationUsers.module.css';
 
 export default function classificationUsers() {
   return(
-    <>
-      <h1>Olá</h1>
-    </>
+    <div className={styles.container}>
+      <h1>Leaderboard</h1>
+      <div>
+        <div className={styles.columns}>
+          <span>Posição</span>
+          <span>Usuário</span>
+          <span>Desafios</span>
+          <span>Experiência</span>
+        </div>
+        <div className={styles.classification}>
+          <p>1</p>
+          <div className={styles.wrapperUserData}>
+
+            <div className={styles.userInformation}>
+              <img src="https://github.com/atalopereira.png" alt="image profile"/>
+              <div>
+                <strong>Átalo Pereira</strong>
+                <p>
+                  <img src="icons/level.svg" alt="level"/>
+                  level 1
+                </p>
+              </div>
+            </div>
+
+            <p> <p>127</p> completados </p>
+            <p> <p>154000</p> xp </p>
+
+          </div>
+        </div>
+        
+      </div>
+    </div>
   );
 }
