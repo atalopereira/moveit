687be7fe414da5fad450df1ae0904464f540308c
atalo pereira <atalo@geowellex.com>
2021-06-08 18:10:03 -0300

    Created graphics page

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -10,6 +10,11 @@ interface SideBarProps {
 export default function SideBar(props: SideBarProps) {
   const route = useRouter();
 
+  function goToGraphics(event) {
+    event.preventDefault();
+    route.push('/graphics');
+  }
+
   function goToHome(event) {
     event.preventDefault();
     route.push('/home');
@@ -28,6 +33,11 @@ export default function SideBar(props: SideBarProps) {
             <img src="icons/logo.svg" alt="logo"/>
           </div>
           <div className={style.options}>
+            <img
+              onClick={goToGraphics}
+              src="icons/graphic.svg"
+              alt="graphics"
+            />
             <img
               onClick={goToHome}
               src="icons/home.svg"
---
+++ src/pages/graphics.tsx
@@ -0,0 +1,9 @@
+import style from '../styles/pages/Graphics.module.css';
+
+export default function Graphics() {
+  return(
+    <div className={style.container}>
+      <h1>Graficos</h1>
+    </div>
+  );
+}
